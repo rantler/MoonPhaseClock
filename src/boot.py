ae22781772888ad04f161b92fdf407a13aadb7cb
Randy Antler <tantalusrur@gmail.com>
2022-08-26 20:48:25 -0700

    Add reference code for future use

--- src/boot.py
+++ src/boot.py
@@ -1,2 +1,16 @@
 import storage
-storage.remount("/", False)
+storage.remount("/", readonly = False)
+# storage.getmount("/").label = "M4_CLOCK"
+# storage.remount("/", readonly = False)
+
+# import os
+# fs_stat = os.statvfs('/')
+# print("Disk size: {0}KB".format(fs_stat[0] * fs_stat[2] // 1024))
+# print("Free space: {0}KB".format(fs_stat[0] * fs_stat[3] // 1024))
+# for pin in dir(microcontroller.pin):
+#     if isinstance(getattr(microcontroller.pin, pin), microcontroller.Pin):
+#         print("".join(("microcontroller.pin.", pin, "\t")), end=" ")
+#         for alias in dir(board):
+#             if getattr(board, alias) is getattr(microcontroller.pin, pin):
+#                 print("".join(("", "board.", alias)), end=" ")
+#     print()
