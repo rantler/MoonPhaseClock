dc381fe50773a3d170597702cbd2ddb45427d280
Randy Antler <tantalusrur@gmail.com>
2023-09-17 15:03:50 -0700

    Add example secrets.py file

---
+++ src/secrets-example.py
@@ -0,0 +1,10 @@
+# This file is where you keep WiFi settings, passwords, and other settings.
+#
+# DO NOT CHECK THIS FILE INTO SOURCE CONTROL!
+#
+secrets = {
+    'sleep_hour': 0,
+    'wake_hour': 6,
+    'latitude': 47.608013,
+    'longitude': -122.335167
+}
